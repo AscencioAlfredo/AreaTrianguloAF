704f4d348dd02fbd6a09d7bc6431aabd78cfb5f4
ascencioalfredo <ascencioalfredo536@gmail.com>
2023-08-28 19:40:16 -0600

    Agregar archivos de proyecto.

---
+++ AreaTrianguloAF/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+namespace AreaTrianguloAF;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void CalcularButton_Clicked(object sender, EventArgs e)
+    {
+        if (double.TryParse(LadoAEntry.Text, out double ladoA) &&
+            double.TryParse(LadoBEntry.Text, out double ladoB) &&
+            double.TryParse(LadoCEntry.Text, out double ladoC))
+        {
+            double semiperimetro = (ladoA + ladoB + ladoC) / 2;
+            double area = Math.Sqrt(semiperimetro * (semiperimetro - ladoA) * (semiperimetro - ladoB) * (semiperimetro - ladoC));
+
+            ResultadoLabel.Text = $"Área: {area:F2}";
+        }
+        else
+        {
+            ResultadoLabel.Text = "Ingrese longitudes válidas para los lados.";
+        }
+    }
+}
+	
+
+	
+
+
